e3f88e1cc01d546dbdae5f18f611367a9b919636
Quốc Bảo <baokiinkk@gmail.com>
2021-05-20 12:15:18 +0700

    xóa dư thừa

--- app/src/main/java/com/baokiin/mangatoon/ui/activity/MainActivity.kt
+++ app/src/main/java/com/baokiin/mangatoon/ui/activity/MainActivity.kt
@@ -1,23 +1,9 @@
 package com.baokiin.mangatoon.ui.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.view.WindowManager
-=======
-import android.util.Log
->>>>>>> feature/180521_clean_code_login
-import coil.load
 import com.baokiin.mangatoon.R
-import com.baokiin.mangatoon.utils.Utils.USER
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
